1a5907765952aa14b9bec2ff005577098eabcea9
elvisgj <elvisgjevori@protonmail.com>
2020-12-20 18:41:47 +0100

    design change

--- src/home.js
+++ src/home.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
 
 function Home() {
-
   return (
-
-    <div className='project-link'>
-      <Link className='project' to='/github'>
+    <div className="project-link">
+      <Link className="project" to="/github">
         <h3 className="link">Get GitHub user Repository</h3>
-        <div className="app-img-div">
-          <img src="./github.png" className="app-img" alt="github app"></img>
-        </div>
+        <img src="./github.png" className="app-img" alt="github app"></img>
       </Link>
-      <Link className='project' to='/todo'>
+      <Link className="project" to="/todo">
         <h3 className="link">To Do App</h3>
         <div className="app-img-div">
           <img src="./todo.png" className="app-img" alt="todo app"></img>
         </div>
       </Link>
-      <Link className='project' to='/form'>
+      <Link className="project" to="/form">
         <h3 className="link">Form Validation</h3>
         <div className="app-img-div">
-          <img src="./formValid.png" className="app-img" alt="form validaion app"></img>
+          <img
+            src="./formValid.png"
+            className="app-img"
+            alt="form validaion app"
+          ></img>
         </div>
       </Link>
-    </div >
-
-  )
+    </div>
+  );
 }
 
 export default Home;
