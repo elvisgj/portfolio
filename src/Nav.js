285bd926aea4038a1ecb6c5cfdc204407cc544af
elvisgj <elvisgjevori@protonmail.com>
2021-02-26 15:58:45 +0100

    title change test

--- src/Nav.js
+++ src/Nav.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import './App.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
-
-      return (
-            <nav>
-                  <Link className='h3' to='/'>
-                        <h3 >Elvis Gjevori's works</h3>
-                  </Link>
-            </nav>
-
-      )
+  return (
+    <nav>
+      <Link className="h3" to="/">
+        <h3>Elvis Gjevori's workss</h3>
+      </Link>
+    </nav>
+  );
 }
 
 export default Nav;
